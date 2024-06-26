476c16131d03d8649d70fcbc4bf5f894b4908946
hellotimo <taobluesky@qq.com>
2024-06-26 14:44:02 +0800

    add real api authorization

--- amplify/data/custom-authorizer.ts
+++ amplify/data/custom-authorizer.ts
@@ -2,6 +2,40 @@
 
 // This is sample code. Update this to suite your needs
 import type { AppSyncAuthorizerHandler } from 'aws-lambda'; // types imported from @types/aws-lambda
+import fetch from 'node-fetch';
+
+interface ApiResponse {
+  verified: boolean;
+}
+
+const url = ' https://concordbus2-dev.vetrackr.com/oauth/verify';
+
+const verifyToken = async (token:String): Promise<boolean> => {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        toekn: token
+      })
+    });
+
+    if (!response.ok) {
+      // throw new Error(`HTTP error! status: ${response.status}`);
+      return false;
+    }
+
+    const data: ApiResponse = await response.json() as ApiResponse;
+    console.log('Response:', data);
+    return data.verified;
+
+  } catch (error) {
+    console.error('Error:', error);
+    return false;
+  }
+};
 
 type ResolverContext = {
   userid: string;
@@ -17,8 +51,10 @@ export const handler: AppSyncAuthorizerHandler<ResolverContext> = async (
     authorizationToken,
     requestContext: { apiId, accountId }
   } = event;
+
+  const verified = await verifyToken(authorizationToken);
   const response = {
-    isAuthorized: authorizationToken === 'test-token123',
+    isAuthorized: verified,  //authorizationToken === 'test-token123',
     resolverContext: {
       // eslint-disable-next-line spellcheck/spell-checker
       userid: 'user-id',
